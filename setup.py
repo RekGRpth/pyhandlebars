28eaee143227375de2dd4fcaff28d03351397f24
RekGRpth <rekgrpth@gmail.com>
2021-12-21 10:39:03 +0500

    op

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup, Extension
-
-module = Extension(
-    "_pyhandlebars",
-    ["pyhandlebars.i", "pyhandlebars.c"],
-    libraries=["handlebars"],
-)
+from distutils.core import Extension, setup
 
 setup(
-    ext_modules=[module],
+    author_email='RekGRpth@gmail.com',
+    author='RekGRpth',
+    ext_modules=[Extension(
+        libraries=["handlebars"],
+        name="_pyhandlebars",
+        sources=["pyhandlebars.i", "pyhandlebars.c"],
+    )],
+    license='MIT',
     name="pyhandlebars",
     py_modules=["pyhandlebars"],
     url="https://github.com/RekGRpth/pyhandlebars",
